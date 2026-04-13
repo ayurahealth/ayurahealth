dc8c8cce8ca7a23448d70df39a9089f87610d690
Abhishek Mudgal <Abhishek.M.333@icloud.com>
2026-04-13 23:09:32 +0900

    fix(prod): definitive syntax restoration [main-ok]

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, 
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   skipTrailingSlashRedirect: true,
 
   async headers() {
